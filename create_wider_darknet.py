fb86671f804403d44b33588da6f51bc1196772cd
fabito <fuechi@ciandt.com>
2017-12-26 21:58:46 -0200

    cleaning up tmp dir before tests and moving txt files to labels dir

---
+++ create_wider_darknet.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+r"""Convert raw WIDER dataset to Darknet format for object_detection.
+Example usage:
+    python create_wider_darknet.py \
+        --data_dir=/home/user/wider \
+        --output_dir=/home/user/wider/tf_records
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import logging
+import argparse
+
+from wider import Wider
+from wider.darknet_exporter import DarknetExporter
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', dest='data_dir', action='store', required=True, help='')
+    parser.add_argument('--output_dir', dest='output_dir', action='store', required=True, help='')
+    args = parser.parse_args()
+    data_dir = args.data_dir
+    output_dir = args.output_dir
+    wider = Wider(data_dir)
+    exporter = DarknetExporter(wider)
+    exporter.export(output_dir)
+
+
+if __name__ == '__main__':
+    main()
